2306ac3c296188c9a9c9b239b2aae9aa118ed96e
HarisCodeNinja <haris.shahid@codeninjaconsulting.com>
2025-10-29 17:13:41 +0500

    Updated env variables

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,20 +1,19 @@
-import { ILanguage } from "@/interface/common";
+import { ILanguage } from '@/interface/common';
 
 const appConfig = {
-    BASE_URL: 'http://localhost:8000',
-    FILE_PATH: 'http://localhost:8000/public/',
-    ENVIRONMENT : 'development',
-    PERSIST_STORE_NAME : 'boilerplate',
-	CACHE_TOKEN: '3002910e-c28d-45c2-9ce5-edf9b8831724'
+  BASE_URL: 'https://hrms-live-backend.hyperapp.site',
+  FILE_PATH: 'https://hrms-live-backend.hyperapp.site/public/',
+  ENVIRONMENT: 'development',
+  PERSIST_STORE_NAME: 'boilerplate',
+  CACHE_TOKEN: '3002910e-c28d-45c2-9ce5-edf9b8831724',
+};
 
-}
+export const AVAILABLE_LANGUAGES: ILanguage[] = [
+  {
+    code: 'en',
+    dir: 'ltr',
+  },
+];
 
-export const AVAILABLE_LANGUAGES : ILanguage [] = [
-	{
-		 "code": "en",
-		 "dir" : "ltr"
-	}
-]
-
-export const {ENVIRONMENT, PERSIST_STORE_NAME,BASE_URL, FILE_PATH, CACHE_TOKEN} = appConfig;
+export const { ENVIRONMENT, PERSIST_STORE_NAME, BASE_URL, FILE_PATH, CACHE_TOKEN } = appConfig;
 export default appConfig;
